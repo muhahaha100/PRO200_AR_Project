90811bb5e806a9d7e082456894779e61616c994d
cwllight8 <cwllight8@gmail.com>
2020-11-13 19:50:36 -0700

    gps code and updated android build stuff

---
+++ AR Project/Assets/Scripts/GPSTextTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSTextTest : MonoBehaviour
+{
+    public Text Coordinates;
+
+    private void Update()
+    {
+        Coordinates.text = "lat: " + GPS.Instance.lat.ToString() + "  long: " + GPS.Instance.lon.ToString();
+    }
+}
