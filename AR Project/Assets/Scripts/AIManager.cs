f0d34501c0ddfdc69ce4618181bb9ca78cd181ab
troy82086 <45493654+troy82086@users.noreply.github.com>
2020-11-16 18:17:57 -0700

    Start of Scripts

---
+++ AR Project/Assets/Scripts/AIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIManager : MonoBehaviour
+{
+
+
+    
+    public void Think(Entity[] entities)
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
