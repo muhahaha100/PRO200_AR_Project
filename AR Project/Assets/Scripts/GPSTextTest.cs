74c80341286f612e369412c62c1e8ece516a4d0c
cwllight8 <cwllight8@gmail.com>
2020-11-18 18:26:53 -0700

    no internet day stuffs

--- AR Project/Assets/Scripts/GPSTextTest.cs
+++ AR Project/Assets/Scripts/GPSTextTest.cs
@@ -9,6 +9,6 @@ public class GPSTextTest : MonoBehaviour
 
     private void Update()
     {
-        Coordinates.text = "lat: " + GPS.Instance.lat.ToString() + "  long: " + GPS.Instance.lon.ToString();
+        Coordinates.text = "lat: " + GPS.Instance.lat.ToString() + "  long: " + GPS.Instance.lon.ToString() + " end";
     }
 }
