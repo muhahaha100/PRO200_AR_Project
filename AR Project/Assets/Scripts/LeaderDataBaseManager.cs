5fe7bea57041a203e2493900eb6449da3c0ec3a1
cwllight8 <cwllight8@gmail.com>
2020-12-02 19:52:00 -0700

    score saving implemented

--- AR Project/Assets/Scripts/LeaderDataBaseManager.cs
+++ AR Project/Assets/Scripts/LeaderDataBaseManager.cs
@@ -1,7 +1,9 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class LeaderDataBaseManager : MonoBehaviour
@@ -13,10 +15,49 @@ public class LeaderDataBaseManager : MonoBehaviour
     {
         database = client.GetDatabase("5minus1");
         collection = database.GetCollection<BsonDocument>("scoreBoard");
+        Debug.Log("started");
+        //SaveScoreToDataBase("cwll", 10);
+        
+        //GetScoresFromDataBase();
     }
 
-    public async void SaveScoreToDataBase(string username, int score, float lon, float lat)
+    public async void SaveScoreToDataBase(string username, int score)
     {
-        BsonDocument document = new BsonDocument { { username, score } };
+        BsonDocument document = new BsonDocument { {"username", username }, {"score", score }, {"city", GPS.Instance.city } };
+
+        await collection.InsertOneAsync(document);
     }
+
+    public async Task<List<PlayerScore>> GetScoresFromDataBase()
+    {
+        FilterDefinition<BsonDocument> filter = new BsonDocument("city", "SLC");
+        var localScores = await collection.FindAsync(filter);
+        
+
+        List<PlayerScore> highScores = new List<PlayerScore>();
+        foreach (var score in localScores.ToList())
+        {
+            highScores.Add(Deserialize(score));
+        }
+
+        return highScores;
+    }
+
+    private PlayerScore Deserialize(BsonDocument raw)
+    {
+        PlayerScore playerScore = new PlayerScore();
+
+        playerScore.UserName = raw[1].ToString();
+        playerScore.Score = raw[2].ToInt32();
+        playerScore.City = raw[3].ToString();
+
+        return playerScore;
+    }
+}
+[Serializable]
+public class PlayerScore
+{
+    public string UserName { get; set; }
+    public int Score { get; set; }
+    public string City { get; set; }
 }
