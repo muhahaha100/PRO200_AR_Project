1907827a3a5860cafd406590d6a6809bb92daee4
Vernon Howerton <vhowerton@student.neumont.edu>
2020-12-09 19:34:36 -0700

    Added Pause Menu to Game Scene
    Read Title

---
+++ AR Project/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject PauseButton = null;
+    [SerializeField] GameObject PauseMenu = null;
+
+    public void OpenMenu()
+    {
+        PauseButton.SetActive(false);
+        PauseMenu.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        PauseButton.SetActive(true);
+        PauseMenu.SetActive(false);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("StartupScene");
+    }
+
+}
